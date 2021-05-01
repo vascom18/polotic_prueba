1a95d6aee76cba84bbb1e62dadd3cbdc80426b53
Martín Andrés Picchi <martinpicchi@gmail.com>
2021-05-01 00:47:48 -0300

    se agregan 2 ejercicios

---
+++ caso2a.py
@@ -0,0 +1,13 @@
+# Defino variables
+disco1 = "500 gb"
+disco2 = "1 tb"
+disco3 = "2 tb"
+memoria1 = "4 gb"
+memoria2 = "8 gb"
+memoria3 = "16 gb"
+procesador1 = "Ryzen3"
+procesador2 = "Ryzen5"
+procesador3 = "Ryzen7"
+
+# Imprimo un cadena con llaves vacias y al final aplico .format()
+print("Los componentes de la pc 2 son: disco de {}, memoria de {} y procesador {}".format(disco2,memoria2,procesador2))
---
+++ caso2b.py
@@ -0,0 +1,11 @@
+# Ingreso variables por consola
+
+print("Elegí las partes de tu pc")
+print("Opciones para tu disco: 1tb 2tb")
+disco = input("Candidad de gb para el disco: ")
+print("Opciones para la memoria RAM: 8gb 16gb")
+memoria = input("Tamaño de memoria RAM: ")
+print("Opciones para procesador: i5 i7")
+procesador = input("Procesador favorito: ")
+
+print("La pc que elegiste es: un disco de {}, memoria {} y procesador {}".format(disco,memoria,procesador))
